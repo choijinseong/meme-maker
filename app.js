3fb9af689e7f395003ef7303bd0edb78c3db52da
choijinseong <cjs951218@naver.com>
2023-01-31 22:30:16 +0900

    painting board part.1

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
+const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.fill();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function onMouseDown() {
+  isPainting = true;
+}
+
+function onMouseUp() {
+  isPainting = false;
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("mouseleave", onMouseUp);
+
+lineWidth.addEventListener("change", onLineWidthChange);
